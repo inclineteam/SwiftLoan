06011225bbe25f8c2e069bca6605dd40db20c71d
ishmael cascabel <cascabel_ishmael@yahoo.com>
2023-07-20 21:11:06 +0800

    laravel breeze setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./resources/js/**/*.{js,jsx,tsx,ts}"],
+  content: [
+    "./resources/js/**/*.{js,jsx,tsx,ts}",
+    "./resources/**/*.blade.php",
+],
   theme: {
     extend: {},
   },
